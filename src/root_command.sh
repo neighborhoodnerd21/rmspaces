1cb3d1fbda1fc2831249e88c36bf1a7bbe42ea55
NeighborhoodNerd21 <neighborhoodnerd21@gmail.com>
2026-05-07 12:28:10 -0400

    Add files via upload

--- src/root_command.sh
+++ src/root_command.sh
@@ -10,73 +10,43 @@ period="${args[--period]}"
 
 if [ -p /dev/stdin ]; then
   readarray -t files
-  for f in "${files[@]}"; do
-    if [[ -z "$f" ]]; then
-      echo "Error: file names cannot be empty." >&2
-      exit 1
-    elif [[ -d "$f" ]]; then
-      echo "Error: '$f' is a directory" >&2
-      exit 1
-    else
-      fext=".${f##*.}"
-      fnoext="${f%.*}"
-      if [[ $dash ]]; then
-        # shellcheck disable=SC2001
-        new_name=$(echo "${fnoext}" | sed 's/[^A-Za-z0-9-]/-/g')
-        new_name="${new_name}${fext}"
-      elif [[ $underscore ]]; then
-        # shellcheck disable=SC2001
-        new_name=$(echo "${fnoext}" | sed 's/[^A-Za-z0-9_]/_/g')
-        new_name="${new_name}${fext}"
-      elif [[ $period ]]; then
-        # shellcheck disable=SC2001
-        new_name=$(echo "${fnoext}" | sed 's/[^A-Za-z0-9.]/./g')
-        new_name="${new_name}${fext}"
-      else
-        # shellcheck disable=SC2001
-        new_name=$(echo "${fnoext}" | sed 's/[^A-Za-z0-9]//g')
-        new_name="${new_name}${fext}"
-      fi
-    fi
-    mv "$f" "$new_name"
-    echo "File: '$f' was moved to $new_name"
-  done
 elif [[ -n "${args[source]}" ]]; then
   IFS=',' read -ra files <<<"${args[source]}"
-  for f in "${files[@]}"; do
-    if [[ -z "$f" ]]; then
-      echo "Error: file names cannot be empty." >&2
-      exit 1
-    elif [[ -d "$f" ]]; then
-      echo "Error: '$f' is a directory." >&2
-      exit 1
-    else
-      fext=".${f##*.}"
-      fnoext="${f%.*}"
-      if [[ $dash ]]; then
-        # shellcheck disable=SC2001
-        new_name=$(echo "${fnoext}" | sed 's/[^A-Za-z0-9-]/-/g')
-        new_name="${new_name}${fext}"
-      elif [[ $underscore ]]; then
-        # shellcheck disable=SC2001
-        new_name=$(echo "${fnoext}" | sed 's/[^A-Za-z0-9_]/_/g')
-        new_name="${new_name}${fext}"
-      elif [[ $period ]]; then
-        # shellcheck disable=SC2001
-        new_name=$(echo "${fnoext}" | sed 's/[^A-Za-z0-9.]/./g')
-        new_name="${new_name}${fext}"
-      else
-        # shellcheck disable=SC2001
-        new_name=$(echo "${fnoext}" | sed 's/[^A-Za-z0-9]//g')
-        new_name="${new_name}${fext}"
-      fi
-    fi
-    mv "$f" "$new_name"
-    echo "File: '$f' was moved to $new_name"
-  done
 else
   echo "Error: No valid input provided. Please provide a file name."
   exit 1
 fi
+
+for f in "${files[@]}"; do
+  if [[ -z "$f" ]]; then
+    echo "Error: file names cannot be empty." >&2
+    exit 1
+  elif [[ -d "$f" ]]; then
+    echo "Error: '$f' is a directory" >&2
+    exit 1
+  else
+    fext=".${f##*.}"
+    fnoext="${f%.*}"
+    if [[ $dash ]]; then
+      # shellcheck disable=SC2001
+      new_name=$(echo "${fnoext}" | sed 's/[^A-Za-z0-9-]/-/g')
+      new_name="${new_name}${fext}"
+    elif [[ $underscore ]]; then
+      # shellcheck disable=SC2001
+      new_name=$(echo "${fnoext}" | sed 's/[^A-Za-z0-9_-]/_/g')
+      new_name="${new_name}${fext}"
+    elif [[ $period ]]; then
+      # shellcheck disable=SC2001
+      new_name=$(echo "${fnoext}" | sed 's/[^A-Za-z0-9.-]/./g')
+      new_name="${new_name}${fext}"
+    else
+      # shellcheck disable=SC2001
+      new_name=$(echo "${fnoext}" | sed 's/[^A-Za-z0-9-]//g')
+      new_name="${new_name}${fext}"
+    fi
+  fi
+  mv "$f" "$new_name"
+  echo "File: '$f' was moved to $new_name"
+done
 
 exit 0
